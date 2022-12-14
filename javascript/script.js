c7402cc4c4fbddff29f919876d16695135bd4ce1
jacobbergendorff <jacob_bergendorff@hotmail.com>
2022-12-14 10:16:16 +0100

    Test commit

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+//Definition of all id:s/const
+
+//Database containing user names
+
+//Function | Login | Create new account
+
+//Function | Login | Create new account | New create new account interface
+
+//Function | Login | Create new account | Click "Create new account" button 
+
+
+
+//Function | Login | 
+
+
+
