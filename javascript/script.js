05eb04fdc04db1165d8f5c07f93c3b4cd03b4fcd
jacobbergendorff <jacob_bergendorff@hotmail.com>
2022-12-20 20:09:23 +0100

    Update script.js

---
+++ javascript/script.js
@@ -0,0 +1,49 @@
+// Definitions of items relating to the login and registration forms
+let signInButton = document.getElementById('sign-in-button');
+let signUpButton = document.getElementById('sign-up-button');
+let logInButton = document.getElementById('log-in-button');
+let registerButton = document.getElementById('register-button');
+let signInForm = document.getElementById('sign-in-form');
+let signUpForm = document.getElementById('sign-up-form');
+
+// Array with usernames
+var users = [
+    {
+        username: 'admin',
+        password: 'admin'
+    },
+    {
+        username: 'user1',
+        password: 'user1'
+    },
+    {
+        username: 'user2',
+        password: 'user2'
+    },
+    {
+        username: 'user3',
+        password: 'user3'
+    },
+    {
+        username: 'janne',
+        password: 'test'
+    }
+];
+
+// Function that displays a login form after pressing sign in button
+
+// Function that displays a registration form after pressing sign up button
+
+// Function that adds a new user to the users array after pressing the register button
+
+// Function that validates the username and password after pressing the login button - executing login
+
+// Function that displays an error message if the username or password is incorrect
+
+// Function that saves login in localStorage while the browser is open
+
+// Function that removes login from localStorage when the user logs out
+
+// Function that displays new content after logging in
+    // Function that displays log-out button after logging in
+    // Function that displays 
