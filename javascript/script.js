61ae387abc3b03e822d71dc617eeab0a9842c02d
jacobbergendorff <jacob_bergendorff@hotmail.com>
2022-12-27 17:57:15 +0100

    . changes to the login function enabling for more users

--- javascript/script.js
+++ javascript/script.js
@@ -1,63 +1,63 @@
+// Definitions
+const loginContainer = document.getElementById("login-container");
+const content = document.getElementById("content");
+const contentText = document.getElementById("content-text");
+const contentHeading = document.getElementById("content-heading");
+
+const usernameEnter = document.getElementById("username-enter");
+const passwordEnter = document.getElementById("password-enter");
+
+const loginButton = document.getElementById("login-button");
+const logoutButton = document.getElementById("logout-button");
+
 // Array containing usernames
-var userList = [
-    {username: "janne", password: "test"},
-    {username: "janne1", password: "test1"},
-    {username: "janne2", password: "test2"},
-    {username: "janne3", password: "test3"},
-    {username: "janne4", password: "test4"},
-    {username: "janne5", password: "test5"}
+const userList = [
+    {id: 1, username: 'janne', password: 'test'},
+    {id: 2, username: 'janne1', password: 'test1'},
+    {id: 3, username: 'janne2', password: 'test2'},
+    {id: 4, username: 'janne3', password: 'test3'},
+    {id: 5, username: 'janne4', password: 'test4'},
+    {id: 6, username: 'janne5', password: 'test5'}
 ];
 
-// Definitions of items relating to the login and registration forms
-const loginContainer = document.getElementById('login-container');
-const content = document.getElementById('content');
-const contentText = document.getElementById('content-text');
-const contentHeading = document.getElementById('content-heading');
+// Login function 
 
-const usernameEnter = document.getElementById('username-enter');
-const passwordEnter = document.getElementById('password-enter');
+loginButton.addEventListener('click', validateLogin);
 
-const loginButton = document.getElementById('login-button');
-const logoutButton = document.getElementById('logout-button');
+function validateLogin() {
+    const username = usernameEnter.value;
+    const password = passwordEnter.value;
 
+    for (const user of userList) {
+        if (user.username === username && user.password === password) {
+            localStorage.setItem("Username", username);
+            localStorage.setItem("Password", password);
 
-// Function that executes login 
-loginButton.addEventListener("click", executeLogin);
+            alert("Login Successful!");
 
-function executeLogin() {
-    for (var i = 0; i < userList.length; i++) {
-        if (usernameEnter.value === userList[i].username && passwordEnter.value === userList[i].password) {
+            login();
+            break;
+        } 
+    }
+    alert("Login Failed!")
+};
 
-            localStorage.setItem("Username", usernameEnter.value);
-            localStorage.setItem("Password", passwordEnter.value);
+function login() {
+    loginContainer.style.display = "block";
+    content.style.display = "none";
+    logoutButton.style.display = "none";
+};
 
-            loginContainer.style.display = "none";
-            content.style.display = "block";
-            contentHeading.insertAdjacentHTML("beforeend", " " + usernameEnter.value + "!");
-            logoutButton.style.display = "block";
-
-            alert("Welcome to Global Internet Solutions " + usernameEnter.value + "!");
-
-            return true;
-
-        } else {
-
-            alert("Login failed! Try again " + usernameEnter.value + "!");
-
-            return false;
-}}};
-    
 logoutButton.addEventListener("click", executeLogout);
 
 function executeLogout() {
 
     localStorage.clearItem("Username", usernameEnter.value);
     localStorage.clearItem("Password", passwordEnter.value);
-    
-    loginContainer.style.display = "block";
-    content.style.display = "none";
-    logoutButton.style.display = "none";
+
+    loginContainer.style.display = "none";
+    content.style.display = "block";
+    contentHeading.insertAdjacentHTML("beforeend", " " + usernameEnter.value + "!");
+    logoutButton.style.display = "block";
+
 };
-
-
-
